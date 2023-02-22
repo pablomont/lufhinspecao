f7cff1d6ae2aced38ce71c1935eca01799cb9797
pablomonteiro <paabloms@live.com>
2023-02-22 02:41:51 -0300

    fixed swagger

--- src/main/java/com/uepb/lufh/avalia/entrypoint/Application.java
+++ src/main/java/com/uepb/lufh/avalia/entrypoint/Application.java
@@ -3,6 +3,7 @@ package com.uepb.lufh.avalia.entrypoint;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+
 @SpringBootApplication
 public class Application {
     public static void main(String[] args) {
