5010f6f3f8fd6eb71134231908f5b390bf7ce25a
pablomonteiro <paabloms@live.com>
2023-04-21 17:54:36 -0300

    created QuestionEntity end script to insert questions in database

---
+++ src/main/java/com/uepb/lufh/avalia/dataprovider/database/entity/QuestionEntity.java
@@ -0,0 +1,34 @@
+package com.uepb.lufh.avalia.dataprovider.database.entity;
+
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "question")
+@NoArgsConstructor
+public class QuestionEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "question_id")
+    private Long questionId;
+
+    @Column(name = "question_class")
+    private String questionClass;
+
+    @Column(name = "product_type")
+    private String productType;
+
+    @Column(name = "base_question")
+    private String baseQuestion;
+
+    @Column(name = "detailed_question")
+    private String detailedQuestion;
+
+}
